f27ea83edf5551c5a9daffa38ea1b5e0f4b615d2
Giuseppe Simone Aielli <giuseppe@aielli.net>
2020-05-29 15:52:29 +0200

    .

--- OracleCloudDB/Program.cs
+++ OracleCloudDB/Program.cs
@@ -7,13 +7,13 @@ namespace OracleCloudDB
     {
         static void Main(string[] args)
         {
-            OracleConfiguration.OracleDataSources.Add("orclpdb", @"(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1522)(host=adb.eu-zurich-1.oraclecloud.com))(connect_data=(service_name=cfdnkbdx0aohvkj_db202005200815_high.atp.oraclecloud.com))(security=(ssl_server_cert_dn=""CN=adb.eu-zurich-1.oraclecloud.com,OU=Oracle ADB ZURICH,O=Oracle Corporation,L=Redwood City,ST=California,C=US"")))");
+            OracleConfiguration.OracleDataSources.Add("orclpdb", @"<YOUR CONNECTION>");
             OracleConfiguration.WalletLocation = AppContext.BaseDirectory + "OracleWallet";
 
             OracleConnection orclCon = null;
             try
             {
-                orclCon = new OracleConnection("user id=hr; password=.7N3FgQ+!.!z6qr; data source=orclpdb");
+                orclCon = new OracleConnection("user id=hr; password=<YOUR PASSWORD>; data source=orclpdb");
                 orclCon.Open();
                 OracleCommand orclCmd = orclCon.CreateCommand();
                 orclCmd.CommandText = "SELECT FIRST_NAME FROM EMPLOYEES WHERE ROWNUM <= 10";
